ee00e4d2c917e49afa7957d6f6d4aaa7ebe5fa3b
Avishek Neupane <neupaa@ubuntu.(none)>
2013-06-20 09:43:46 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyBuddy::Application.config.secret_token = '8993f09d273f0402138375caff3732e2a5a5c9444d1303203b1a7eb2d27a8bfb7a74d3962fb7a8af32f2959885889c7852f7a7d3f3b8337fe4736635664429ef'
