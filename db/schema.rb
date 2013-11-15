839bbbcab18a3438b0639c82a65c980640741a36
Avishek Neupane <avishekneupane@Avishek-Neupanes-iMac.local>
2013-11-15 16:59:05 -0500

    changed gemfile and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114173021) do
+ActiveRecord::Schema.define(:version => 20131115023614) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(:version => 20131114173021) do
     t.string   "authentication_token"
     t.string   "provider"
     t.string   "uid"
+    t.string   "oauth_token"
+    t.string   "image"
+    t.string   "oauth_expires_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
