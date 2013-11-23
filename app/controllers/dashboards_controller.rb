0465517cb5aa58d56546121dc524ab1c61aca67e
Avishek <avishek@brandeis.edu>
2013-11-23 18:58:30 -0500

    made sure that there are only two activities shown when a user has no courses

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,11 @@ class DashboardsController < ApplicationController
 	def show
 		@courses = Course.where("user_id='#{current_user.id}'")		
 		size = 8
-		if @courses.size < 4
+
+		if @courses.size > 0 && @courses.size < 4 
 			size = @courses.size * 2
+		elsif @courses.size == 0
+			size = 2
 		end
 		@activities = PublicActivity::Activity.order("created_at desc").page(params[:page]).per(size)
 		render "/layouts/dashboard.html.erb"
