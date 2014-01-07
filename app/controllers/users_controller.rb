0930139e96795570eb4e17b9342799a2a9c99efe
Avishek <avishek@brandeis.edu>
2014-01-06 21:53:48 -0500

    changed the message for after confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		Devise.sign_out_all_scopes ? sign_out : sign_out(@user)
 		@user.destroy
-		redirect_to root_path, :notice => "Account Deleted. Thanks For Trying StudyBuddy"
+		redirect_to root_path, :notice => "Account Deleted. Thanks For Trying StudyBuddy :)"
 	end
 end
