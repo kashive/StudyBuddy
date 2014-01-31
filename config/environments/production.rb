84857822c3fbc654df69b0ddf2698502017bc99e
Avishek <kashive>
2014-01-31 00:40:54 -0500

    added the production url in the mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ StudyBuddy::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => 'http://buddy-study.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'study-buddy.me' }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
