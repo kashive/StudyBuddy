46ccce0725d56ad15d7c6813f97f16d57c01b534
Avishek <kashive>
2014-01-31 00:36:32 -0500

    changed the url to heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ StudyBuddy::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => 'http://www.study-buddy.me' }
+  config.action_mailer.default_url_options = { :host => 'http://buddy-study.herokuapp.com' }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
