ddcf94a0d0642f3bd2de6da84ef2d8ee853d4911
Avishek <kashive>
2014-01-31 00:26:44 -0500

    added host in the production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ StudyBuddy::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => 'http://www.study-buddy.me' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
