fe986441fadb730d2be679682c68bd9d5d31d4cd
Avishek <avishek.neupane@gmail.com>
2014-03-10 18:55:52 -0400

    added chat css in the assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ StudyBuddy::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += ['chat.css']
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
